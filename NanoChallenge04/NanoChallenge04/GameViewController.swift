f59817d34a09c50b7005d3f024085f92795c5383
FelipeNishino <49385633+FelipeNishino@users.noreply.github.com>
2020-06-02 18:20:55 -0300

    Main menu(sem visual)

--- NanoChallenge04/NanoChallenge04/GameViewController.swift
+++ NanoChallenge04/NanoChallenge04/GameViewController.swift
@@ -17,14 +17,14 @@ class GameViewController: UIViewController {
         
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        if let scene = GKScene(fileNamed: "MenuScene") {
             
             // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
+            if let sceneNode = scene.rootNode as! MenuScene? {
                 
                 // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
+//                sceneNode.entities = scene.entities
+//                sceneNode.graphs = scene.graphs
                 
                 // Set the scale mode to scale to fit the window
                 sceneNode.scaleMode = .aspectFill
