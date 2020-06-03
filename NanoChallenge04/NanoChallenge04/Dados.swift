fc04ca4267d66961f7612ebefd32789226ce0856
GustavoYamauchi <yamauchi_gu@hotmail.com>
2020-06-03 16:37:30 -0300

    UserDefaults + Classe de Dados
    Salvando no UserDefaults o recorde, creditos e upgrade.
    
    Criado a Classe de Dados para manipular os dados do UserDefaults

---
+++ NanoChallenge04/NanoChallenge04/Dados.swift
@@ -0,0 +1,38 @@
+//
+//  Dados.swift
+//  NanoChallenge04
+//
+//  Created by Gustavo Yamauchi on 03/06/20.
+//  Copyright © 2020 Grupo14. All rights reserved.
+//
+
+import Foundation
+
+
+
+class Dados {
+    
+    public var Creditos: Int?
+    public var Recorde: Int?
+    public var Upgrade: Int?
+    
+    
+    init() {
+        
+    }
+    
+    public func carregarDados(){
+        let dadosCarregar : [String:String] = UserDefaults.standard.dictionary(forKey: "Dados") as! [String : String]
+        
+        self.Creditos = Int(dadosCarregar["Creditos"]!)
+        self.Recorde  = Int(dadosCarregar["Recorde"]!)
+        self.Upgrade  = Int(dadosCarregar["Upgrade"]!)
+    }
+    
+    public func salvarDados(){
+        let dadosSalvar : [String:String] = ["Creditos": String(self.Creditos!), "Recorde": String(self.Recorde!), "Upgrade": String(self.Upgrade!)]
+        
+        UserDefaults.standard.set(dadosSalvar, forKey: "Dados")
+    }
+    
+}
